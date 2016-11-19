b48eb22637011bc0867981353c6f4f7f3ef92102
Izaac Zavaleta <izavaleta@nearsoft.com>
2016-11-19 11:08:55 -0700

    Placing all the needed files for scraping

---
+++ bjjheroes/spiders/basic.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.selector import Selector
+from scrapy.http import Request
+
+from bjjheroes.items import BjjheroesItem
+
+base_url = 'http://www.bjjheroes.com'
+
+class BasicSpider(scrapy.Spider):
+    name = "basic"
+    allowed_domains = ["bjjheroes.com"]
+    start_urls = (
+        'http://www.bjjheroes.com/a-z-bjj-fighters-list',
+    )
+
+    def parse(self, response):
+
+        sel = Selector(response)
+        rows = sel.xpath('//tr[position()>1]')
+
+        for row in rows:
+            item = BjjheroesItem()
+
+            # name
+            item['name'] = row.xpath('./td[1]/a/text()').extract()[0]
+
+            # last name
+            item['lastname'] = row.xpath('./td[2]/a/text()').extract()[0]
+
+            # url
+            url = row.xpath('./td[1]/a/@href').extract()
+            url = str(url[0])
+            if not url.startswith('http'):
+                url = base_url + url
+            request = Request(url, callback=self.parse_info)
+            request.meta['item'] = item
+            yield request
+
+
+    def parse_info(self, response):
+        item = response.meta['item']
+        item['url'] = response.url
+        sel = Selector(response)
+        raw = sel.xpath('//*[@id="post-2729"]/div/*/text()').extract()
+        content =  [x for x in raw if x != '\n' and x != ' ' and '\r\n' not in x and x != '\n \n \n']
+        item['content'] = content
+        yield item
